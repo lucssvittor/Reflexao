2092c3e502f48c85a8ce486c54945a0e18a14369
Lucas Vitor <166927209+lucssvittor@users.noreply.github.com>
2024-12-07 14:04:14 -0300

    Add files via upload

---
+++ Reflexão/Jesus.java
@@ -0,0 +1,36 @@
+public class JesusReflection {
+    
+    public static void main(String[] args) {
+
+        System.out.println("Jesus: O Caminho, a Verdade e a Vida");
+
+        String caminho = "Amor";
+        String destino = "Vida Eterna";
+        System.out.println("Caminho: " + caminho);
+        System.out.println("Destino: " + destino);
+
+
+        String[] ensinos = {"Amar a Deus acima de tudo", "Amar ao próximo como a ti mesmo", "Perdoar sempre"};
+        System.out.println("Ensinos de Jesus:");
+        for (String ensino : ensinos) {
+            System.out.println("- " + ensino);
+        }
+
+
+        int sacrificio = 1;
+        if (sacrificio == 1) {
+            System.out.println("Jesus entregou sua vida por nós.");
+            System.out.println("Ele se sacrificou para que pudéssemos ser libertos dos nossos pecados.");
+        }
+
+        String amor = "incondicional";
+        System.out.println("O amor de Jesus é " + amor + ".");
+
+
+        String mensagem = "Jesus nos ensina a sermos humildes, compassivos e a vivermos em harmonia com a vontade de Deus.";
+        System.out.println(mensagem);
+        
+
+        System.out.println("Senhor, que possamos seguir o exemplo de Jesus e refletir seu amor e luz em nossas vidas. Amém.");
+    }
+}
